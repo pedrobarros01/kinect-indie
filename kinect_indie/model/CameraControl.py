f6095935bbb666feda24d6d2f3724850642bc1fe
Pedro Barros <82074537+pedrobarros01@users.noreply.github.com>
2024-12-01 01:54:11 -0300

    feat: desenvolvendo lógica de verficação de pulo e movimetno

--- kinect_indie/model/CameraControl.py
+++ kinect_indie/model/CameraControl.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 from ..model.IModelo import IModelo
+from ..services.GameService import GameService
 class CameraControl:
     def __init__(self, camera_id, modelo: IModelo, resolution: tuple[int, int], exit_key='q') -> None:
         self.camera_id = camera_id
@@ -8,6 +9,8 @@ class CameraControl:
         self.resolution = resolution
         self.line_start = (0,50)
         self.line_end = (self.resolution[1] + 170, 50)
+        self.quad_esq = [(25, 200), (75, 300)]
+        self.quad_dir = [(550, 200), (600, 300)]
         self.line_color = (0,0,255)
         self.thickness = 2
 
@@ -20,17 +23,32 @@ class CameraControl:
             if not status or cv.waitKey(1) & 0xFF == ord(self.exit_key):
                 ligado = False
             detection_results = self.modelo.predizer_frame(frame)
+            points_minimum_player = None
+            points_maximum_player = None
             for detection in detection_results:
                 bounding_boxes = detection.boxes.xyxy
                 for box in bounding_boxes:
                     x_min, y_min, x_max, y_max = map(int, box[:4])
-                    if len(box) > 5:
-                        class_id = int(box[5])
-                        class_label = detection.names[class_id]
-                        cv.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
-                        cv.putText(frame, class_label, (x_min, y_min - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-                    else:
-                        cv.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
+                    '''class_id = int(box[5])
+                    class_label = detection.names[class_id]
+                    if class_label == 'person':'''
+                    cv.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
+                    #cv.putText(frame, class_label, (x_min, y_min - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+                    points_minimum_player = (x_min, y_min)
+                    points_maximum_player = (x_max, y_max)
             
             cv.line(frame, self.line_start, self.line_end, self.line_color, self.thickness)
+            cv.rectangle(frame, (self.quad_esq[0][0], self.quad_esq[0][1]), (self.quad_esq[1][0], self.quad_esq[1][1],), (0,0,255), 2)
+            cv.rectangle(frame, (self.quad_dir[0][0], self.quad_dir[0][1]), (self.quad_dir[1][0], self.quad_dir[1][1],), (0,0,255), 2)
+            if points_minimum_player != None and points_minimum_player != None:
+                if GameService.pulou(points_minimum_player, [self.line_start, self.line_end]):
+                    print('Pulou')
+                elif GameService.mover(points_minimum_player, points_maximum_player, self.quad_esq):
+                    print(points_maximum_player)
+                    print('Moveu pra esq')
+                elif GameService.mover(points_minimum_player, points_maximum_player, self.quad_dir):
+                    print('Moveu pra dir')
+            else:
+                print('Nada')
+           
             cv.imshow("Camera", frame)
--- kinect_indie/services/GameService.py
+++ kinect_indie/services/GameService.py
@@ -1,5 +1,23 @@
 
 class GameService: 
     @classmethod
-    def pulou(cls, points_detect_minimum: tuple[int, int], line_points: list[tuple[int, int]]):
-        pass
+    def pulou(cls, points_detect_minimum: tuple[int, int], line_points: list[tuple[int, int]]) -> bool:
+        _, y_min_player = points_detect_minimum
+        _, y_start_line = line_points[0]
+        _, y_end_line = line_points[1]
+        if y_min_player < y_start_line and y_min_player < y_end_line:
+            return True
+        return False
+    
+
+    @classmethod
+    def mover(cls, points_detect_minimum: tuple[int, int],points_detect_max: tuple[int, int], quad_points: list[tuple[int, int]]) -> bool:
+        x_min_player, y_min_player = points_detect_minimum
+        x_max_player, y_max_player = points_detect_max
+        x_start_quad, y_start_quad = quad_points[0]
+        x_end_quad, y_end_quad = quad_points[1]
+        half_y_max_player = y_max_player // 2
+        if (x_min_player >= x_start_quad and x_min_player <= x_end_quad) or (x_max_player >= x_start_quad and x_max_player <= x_end_quad):
+            return True
+        return False
+    
