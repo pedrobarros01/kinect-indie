bfcae056667757dbd3565636b31be60aa6455f34
Rafael Luna <rafampluna@hotmail.com>
2024-12-02 19:50:09 -0300

    Added key check

--- kinect_indie/controller/main.py
+++ kinect_indie/controller/main.py
@@ -1,7 +1,16 @@
 import cv2 as cv
-from ..model.CameraControl import CameraControl
+from ..model.camera_control import CameraControl
 from ..model.YOLO import ModelYOLO
+from ..services.front_app import coletar_teclas
 
 def run():
-    camera_control = CameraControl(0, ModelYOLO(), (640, 480))
-    camera_control.ligar_camera()
+    teclas = coletar_teclas()
+
+    if teclas:
+        exit_key, left_key, right_key, jump_key = teclas
+        camera_control = CameraControl(0, ModelYOLO(), (640, 480), exit_key, left_key, right_key, jump_key)
+        camera_control.ligar_camera()
+    else:
+        print('Nenhuma tecla foi inserida')
+        camera_control = CameraControl(0, ModelYOLO(), (640, 480), 'q', 'left', 'right', 'space')
+        camera_control.ligar_camera()
